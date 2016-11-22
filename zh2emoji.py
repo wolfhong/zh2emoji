72e4467c941994e47dd6f0fd84d337fdd0fc01c4
wolfhong <xucai.hong@extremevision.mo>
2016-11-22 23:22:48 +0800

    readme

--- zh2emoji.py
+++ zh2emoji.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from __future__ import print_function
 from PIL import Image, ImageDraw, ImageFont
 
 
@@ -62,10 +64,7 @@ if __name__ == '__main__':
     对于emoji表情, 可能跟终端的打印方式有关, 对比后自行决定后面需不需要加上空格填充;
     对于中文,输出正好
     '''
-    print image2print(word2image(u'茴'), u'W ')
-    print image2print(word2image(u'茴'), u'❤️ ')
-    print image2print(word2image(u'茴'), u'茴', width=40)
-    print image2print(word2image(u'狗'), u'🐶 ')
-    print image2print(word2image(u'熊'), u'🐻 ')
-    print image2print(word2image(u'色'), u'😍 ')
-    print image2print(word2image(u'哭'), u'😂 ')
+    print(image2print(word2image(u'茴'), u'W '))
+    print(image2print(word2image(u'茴'), u'❤️ '))
+    print(image2print(word2image(u'茴'), u'茴', width=40))
+    print(image2print(word2image(u'熊'), u'🐻 '))
